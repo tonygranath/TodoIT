45f558c349d32f6d316e130c394e61c46f2f9365
Tony Granath <tony.granath@gmail.com>
2021-10-26 17:28:21 +0200

    Update

--- src/test/java/se/lexicon/tonygranath/model/TodoItem.java
+++ src/test/java/se/lexicon/tonygranath/model/TodoItem.java
@@ -86,5 +86,4 @@ public class TodoItem {
 				+ ",\ndone: " + done
 				+ ",\ncreator: " + creator.getSummary() + " }";
 	}
-
 }
--- src/test/java/se/lexicon/tonygranath/model/TodoItemTest.java
+++ src/test/java/se/lexicon/tonygranath/model/TodoItemTest.java
@@ -3,7 +3,6 @@ package se.lexicon.tonygranath.model;
 import org.junit.Before;
 import org.junit.Test;
 import java.time.LocalDate;
-
 import static org.junit.Assert.*;
 
 public class TodoItemTest {
